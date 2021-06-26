e407353dd3e31a6e225835541b7bfef2a3a58b04
IsraelMitolu <israelmitolu@gmail.com>
2021-06-26 12:49:10 +0100

    Test Light/Dark mode toggle

--- assets/js/app.js
+++ assets/js/app.js
@@ -69,3 +69,26 @@ function fadeOut(){
         ease: Power2.easeInOut
     })
 }
+
+
+
+
+/////Dark theme toggle
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+    if(this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+    } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    }
+})
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
